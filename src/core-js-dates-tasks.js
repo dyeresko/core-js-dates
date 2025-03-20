aa440ad85714842139737a721ea8fcb583ef5278
Denis Yeresko <denis.yeresko@gmail.com>
2025-03-20 14:27:05 +0200

    feat: finish task getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -79,8 +79,13 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const fridayDay = date;
+  do {
+    fridayDay.setDate(date.getDate() + 1);
+  } while (getDayName(fridayDay) !== 'Friday');
+
+  return fridayDay;
 }
 
 /**
