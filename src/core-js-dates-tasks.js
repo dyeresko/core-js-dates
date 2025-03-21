2297d86b56f575b824da20aa8585bb4ef1b55fa8
Denis Yeresko <denis.yeresko@gmail.com>
2025-03-21 02:25:57 +0200

    finish task isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -355,8 +355,13 @@ function getWorkSchedule(period, countWorkDays, countOffDays) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  if (date.getFullYear() % 4 === 0) {
+    if (date % 100 !== 0 || date % 400 === 0) {
+      return true;
+    }
+  }
+  return false;
 }
 
 module.exports = {
